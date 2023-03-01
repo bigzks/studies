e6e72ce87216ad269073f0b350b42d3576e26e77
bigzks <gustavohbhb12@gmail.com>
2023-03-01 13:53:46 -0300

    complete 2 exercises

---
+++ exercises/python/listItemRemove/myAns.py
@@ -0,0 +1,17 @@
+'''
+In this question, You need to remove items from a list while iterating but without creating a different copy of a list.
+
+Remove numbers greater than 50
+'''
+number_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+i = 0
+n = len(number_list)
+while i < n:
+  if number_list[i] > 50:
+    del number_list[i]
+    n -= 1
+    
+  else:
+    i += 1
+
+print(number_list)
