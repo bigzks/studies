88ff3e8c7ef07ff76ba7a824d78f09b8dea8debe
bigzks <gustavohbhb12@gmail.com>
2023-03-10 19:31:32 -0300

    remove exercises/python

--- exercises/python/noNewLine/myAns.py
+++
@@ -1,7 +0,0 @@
-"""
-Read text file into a variable and replace all newlines with space
-"""
-with open('./text.txt') as f:
-  lines = f.read()
-
-print(lines.replace('\n', ' '))
--- exercises/python/noNewLine/websiteAns.py
+++
@@ -1,3 +0,0 @@
-with open('sample.txt', 'r') as file:
-    data = file.read().replace('\n', ' ')
-    print(data)
