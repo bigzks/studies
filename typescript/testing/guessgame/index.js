e42b520da6e2c0026a723929e0634805e52353df
bigzks <gustavohbhb12@gmail.com>
2023-03-10 23:28:02 -0300

    start typescript and angular studies

---
+++ typescript/testing/guessgame/index.js
@@ -0,0 +1,61 @@
+import chalkAnimation from 'chalk-animation';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+const sleep = (time) => new Promise((resolve, reject) => setTimeout(resolve, time));
+var restart;
+async function splashScreen() {
+    console.clear();
+    // guess game animated pixel art
+    let asciiGuessGame = chalkAnimation.rainbow(" ██████  ██    ██ ███████ ███████ ███████      ██████   █████  ███    ███ ███████ \n██       ██    ██ ██      ██      ██          ██       ██   ██ ████  ████ ██      \n██   ███ ██    ██ █████   ███████ ███████     ██   ███ ███████ ██ ████ ██ █████   \n██    ██ ██    ██ ██           ██      ██     ██    ██ ██   ██ ██  ██  ██ ██      \n ██████   ██████  ███████ ███████ ███████      ██████  ██   ██ ██      ██ ███████ ");
+    // wait and stop animation
+    await sleep(3000);
+    asciiGuessGame.stop();
+}
+async function mainGame() {
+    // randomize number
+    let number = Math.ceil(Math.random() * 100);
+    var guess = -1;
+    var KeepPlaying;
+    console.clear();
+    await sleep(800);
+    console.log("Can you guess the", chalk.bgWhite(chalk.blackBright("number")), "I'm thinking?\n");
+    while (guess != number) {
+        // ask for guess and treat not numbers
+        var ask = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'guess',
+                message: "What's your guess? (1 - 100)\n",
+                validate(answer) {
+                    if (isNaN(answer)) {
+                        return console.log(chalk.bgRed("Invalid number!\n"));
+                    }
+                    return true;
+                }
+            }
+        ]);
+        guess = ask.guess;
+        // check if guessed right
+        if (guess != number) {
+            // ask if wants to guess again
+            ask = await inquirer.prompt([
+                {
+                    type: 'confirm',
+                    name: 'KeepPlaying',
+                    message: 'Wrong! Want to continue?\n',
+                    default: true
+                }
+            ]);
+            // if not say number and quit
+            if (!ask.KeepPlaying) {
+                console.log("What a shame, loser! The number was", number, "\n");
+                break;
+            }
+        }
+    }
+}
+async function main() {
+    await splashScreen();
+    await mainGame();
+}
+await main();
